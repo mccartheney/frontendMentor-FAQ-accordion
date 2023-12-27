b382ba860cfd65c79179ba32a95223b33d132ba1
Mccartheney Mendes <mccartheney@hotmail.com>
2023-12-27 18:45:19 +0000

    funcionalities finalized

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,38 @@ var openButtons = document.querySelectorAll(".dropdown_header_button")
 
 openButtons.forEach (button => {
     button.addEventListener("click", () => {
-        if (button.getAttribute("active") == "false") {
-            console.log(button.getAttribute("active"))
-            button.setAttribute("active", "true")
-        } else if (button.getAttribute("active") == "true"){
-            console.log(button.getAttribute("active"))
-            button.setAttribute("active", "false")
-        }
+        switch (button.getAttribute("active")) {
+            case "true":
+                button.style.backgroundColor = "hsl(292, 42%, 14%)";
+                button.setAttribute("active", "false");
+                button.innerHTML = "-"
+                button.parentNode.parentNode.childNodes[3].style.display = "block";
+                break;
         
+            default:
+                button.innerHTML = "+"
+                button.style.backgroundColor = "hsl(292, 16%, 49%)";
+                button.setAttribute("active", "true");
+                button.parentNode.parentNode.childNodes[3].style.display = "none"
+                break;
+        }
     })
 })
+
+// openButtons.forEach(button => {
+//     button.addEventListener("click", () => {
+//         switch (button.getAttribute("active")) {
+//             case "true":
+//                 button.setAttribute("active", "false");
+//                 button.style.backgroundColor = "hsl(292, 42%, 14%)";
+//                 button.parentNode.parentNode.childNodes[3].style.display = "block";
+//                 break;
+
+//             default:
+//                 button.setAttribute("active", "true");
+//                 button.style.backgroundColor = "hsl(292, 16%, 49%)";
+//                 button.parentNode.parentNode.childNodes[3].style.display = "none";
+//                 break;
+//         }
+//     })
+// });
